bdb7b93de1c21d5dbfa7ce02e9d94e90c8224d94
DaviRogs <davirocha12.80@gmail.com>
2025-04-25 23:57:03 -0300

    fix: retirando o ping da apirouter

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,8 +1,3 @@
-# Pequena endpoint para testar, mas deverá ser removida
 from fastapi import APIRouter
 
 router = APIRouter()
-
-@router.get("/ping")
-async def ping():
-    return {"message": "pong"}
